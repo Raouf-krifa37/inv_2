5b231a90f05f77476c831221d25f30a6f4c45e56
Raouf-krifa37 <krifaraouf60@gmail.com>
2026-05-18 02:25:59 +0100

    DB backup 2026-05-18

--- backup.bat
+++ backup.bat
@@ -1,7 +1,7 @@
 @echo off
 set FECHA=%date:~-4%-%date:~3,2%-%date:~0,2%
 
-"C:\Users\bouch\OneDrive\Bureau\mongoDbTools\mongodb-database-tools-windows-x86_64-100.17.0\bin\mongodump" --uri=mongodb://krifaraouf60_db_user:I4mtTu1yZJeI31JP@ac-4uyxbxv-shard-00-00.nmj85vd.mongodb.net:27017,ac-4uyxbxv-shard-00-01.nmj85vd.mongodb.net:27017,ac-4uyxbxv-shard-00-02.nmj85vd.mongodb.net:27017/inventory?ssl=true&replicaSet=atlas-v7i0n0-shard-0&authSource=admin&appName=Cluster0" --out="backup\%FECHA%"
+"C:\Users\bouch\OneDrive\Bureau\mongoDbTools\mongodb-database-tools-windows-x86_64-100.17.0\bin\mongodump" --uri="mongodb+srv://krifaraouf60_db_user:I4mtTu1yZJeI31JP@cluster0.nmj85vd.mongodb.net/?appName=Cluster0" --out="backup\%FECHA%"
 
 git pull origin main
 git add backup\
@@ -11,3 +11,8 @@ git commit -m "DB backup %FECHA%"
 git push origin main
 
 echo Backup done!
+
+
+
+
+
